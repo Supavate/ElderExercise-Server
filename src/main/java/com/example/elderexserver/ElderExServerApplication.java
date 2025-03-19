1721de0b565d4363cf13c42dd591c1536195bfa6
ArtTermProject <termprojectart@gmail.com>
2025-03-19 18:38:06 +0700

    Initial commit

---
+++ src/main/java/com/example/elderexserver/ElderExServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.elderexserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElderExServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElderExServerApplication.class, args);
+    }
+
+}
