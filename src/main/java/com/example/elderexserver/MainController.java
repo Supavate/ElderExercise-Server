9820ca0a26fd5b100df99c7c2d38e51609d263e9
Supavate <supavate@gmail.com>
2025-03-23 18:12:57 +0700

    create query getPatientsByStaff

--- src/main/java/com/example/elderexserver/MainController.java
+++ src/main/java/com/example/elderexserver/MainController.java
@@ -2,6 +2,7 @@ package com.example.elderexserver;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,8 +12,15 @@ public class MainController {
     @Autowired
     private PatientsRepository patientsRepository;
 
-    @GetMapping("/")
+    @GetMapping("/pat")
     public List<Patients> getAllPatients() {
         return patientsRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Patients getPatientById(@PathVariable int id) {return patientsRepository.findById(id).get();}
+
+    @GetMapping("/pat/{id}")
+    public List<Patients> getPatientByStaffID(@PathVariable int id) {return patientsRepository.getPatientsByStaff(id);}
+
 }
--- src/main/java/com/example/elderexserver/PatientsRepository.java
+++ src/main/java/com/example/elderexserver/PatientsRepository.java
@@ -1,6 +1,12 @@
 package com.example.elderexserver;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PatientsRepository extends JpaRepository<Patients, Integer> {
+
+    @Query(value = "select * from Patients p where p.Main_Caretaker=:Staff_ID", nativeQuery = true)
+    List<Patients> getPatientsByStaff(int Staff_ID);
 }
