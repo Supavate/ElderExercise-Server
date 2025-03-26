49d55c36adbdcdf539c2fa3136776ea70d1885f9
Supavate <supavate@gmail.com>
2025-03-26 16:40:31 +0700

    Add GetRequest to test

--- src/main/java/com/example/elderexserver/MainController.java
+++ src/main/java/com/example/elderexserver/MainController.java
@@ -7,31 +7,56 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class MainController {
     @Autowired
-    private PatientsRepository patientsRepository;
+    private PatientRepository patientRepository;
 
     @GetMapping("/pat")
-    public List<Patients> getAllPatients() {
-        return patientsRepository.findAll();
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
     }
 
-    @GetMapping("/{id}")
-    public Patients getPatientById(@PathVariable int id) {return patientsRepository.findById(id).get();}
-
     @GetMapping("/pat/{id}")
-    public List<Patients> getPatientByStaffID(@PathVariable int id) {return patientsRepository.getPatientsByStaff(id);}
+    public Patient getPatientById(@PathVariable int id) {return patientRepository.findById(id).get();}
 
     @GetMapping("/pat/age/{id}")
     public int getPatientAge(@PathVariable int id) {
-        Patients pat = patientsRepository.findById(id).get();
-        return Period.between(pat.getDateOfBirth(), LocalDate.now()).getYears();
+        Patient pat = patientRepository.findById(id).get();
+        return Period.between(pat.getDate_of_birth(), LocalDate.now()).getYears();
     }
 
     @GetMapping("/pat/age")
-    public List<Patients> getPatientAge() {return patientsRepository.getPatientsAge();}
+    public List<Object> getPatientAge() {
+        return patientRepository.getPatientsAge();
+    }
+
+    @Autowired
+    private StaffRepository staffRepository;
+
+    @GetMapping("/pat/staff/{id}")
+    public List<Patient> getPatientsByStaff(@PathVariable int id) {return patientRepository.findByCaretaker(id);}
+
+    @GetMapping("/staff")
+    public List<Staff> getAllStaff() {return staffRepository.findAll();}
+
+    @GetMapping("/staff/{id}")
+    public Staff getStaffById(@PathVariable int id) {return staffRepository.findById(id).get();}
+
+    @GetMapping("/staff/super/{id}")
+    public List<Staff> getStaffBySupervisor(@PathVariable int id) {return staffRepository.findBySupervisor(id);}
+
+    @Autowired
+    RoutineRepository routineRepository;
+
+    @GetMapping("/routine")
+    public List<Routine> getAllRoutines() {return routineRepository.findAll();}
+
+    @Autowired
+    ExerciseRepository exerciseRepository;
+    @GetMapping("/exe")
+    public List<Exercise> getAllExercises() {return exerciseRepository.findAll();}
 }
