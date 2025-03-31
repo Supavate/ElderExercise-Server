6b409d03c695859be6134a5648a227bdec45a626
Supavate <supavate@gmail.com>
2025-03-31 14:06:01 +0700

    Add ActualExerciseDetailRepository

---
+++ src/main/java/com/example/elderexserver/repository/ActualExerciseDetailRepository.java
@@ -0,0 +1,7 @@
+package com.example.elderexserver.repository;
+
+import com.example.elderexserver.exercise.Actual_Exercise_Detail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActualExerciseDetailRepository extends JpaRepository<Actual_Exercise_Detail, Integer> {
+}
