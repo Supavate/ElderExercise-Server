9081d4750d770fc3e38dc4da6451d877903192c9
Supavate <supavate@gmail.com>
2025-04-20 15:32:20 +0700

    Update PatientRepository and PatientService to support PatientWithAge and PatientWithAllergies

--- src/main/java/com/example/elderexserver/repository/PatientRepository.java
+++ src/main/java/com/example/elderexserver/repository/PatientRepository.java
@@ -1,6 +1,8 @@
 package com.example.elderexserver.repository;
 
 import com.example.elderexserver.data.patient.Patient;
+import com.example.elderexserver.data.patient.View.PatientWithAgeView;
+import com.example.elderexserver.data.patient.View.PatientWithAllergiesView;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,15 +10,20 @@ import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
-    @Query(value = "SELECT *, TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) AS age FROM Patient;", nativeQuery = true)
-    List<Patient> findAll();
-
     @Query(value = "SELECT *, TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) AS age FROM Patient p WHERE p.id=:id;", nativeQuery = true)
     Patient findById(int id);
 
     @Query(value = "select * from Patient p WHERE p.caretaker_id=:id", nativeQuery = true)
     List<Patient> findByCaretaker(int id);
 
-    @Query(value = "SELECT id, first_name, last_name, weight, height, TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) AS age FROM Patient p WHERE p.id=:id;", nativeQuery = true)
-    Patient findByIdWithAge(int id);
+    @Query(value = "SELECT id, first_name, last_name, weight, height, TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) AS age FROM patient;", nativeQuery = true)
+    List<PatientWithAgeView> findAllWithAge();
+
+    @Query(value = """
+        Select p.id, first_name, last_name, weight, height, GROUP_CONCAT(a.name) AS allergies from Patient p
+        LEFT JOIN patient_allergy ON patient_allergy.patient_id = p.id
+        LEFT JOIN allergy a ON a.id = patient_allergy.allergy_id
+        GROUP BY p.id;
+    """, nativeQuery = true)
+    List<PatientWithAllergiesView> findAllWithAllergies();
 }
--- src/main/java/com/example/elderexserver/service/PatientService.java
+++ src/main/java/com/example/elderexserver/service/PatientService.java
@@ -1,21 +1,39 @@
 package com.example.elderexserver.service;
 
-import com.example.elderexserver.data.patient.DTOs.PatientWithAge;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import com.example.elderexserver.data.patient.DTO.PatientWithAge;
+import com.example.elderexserver.data.patient.DTO.PatientWithAllergies;
+import com.example.elderexserver.data.patient.View.PatientWithAgeView;
+import com.example.elderexserver.data.patient.View.PatientWithAllergiesView;
+import com.example.elderexserver.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 @Service
 public class PatientService {
-    @PersistenceContext
-    private EntityManager entityManager;
+    @Autowired
+    private PatientRepository patientRepository;
 
-    public List<PatientWithAge> findAllWithAge() {
-        return entityManager.createNativeQuery("""
-            SELECT id, first_name, last_name, weight, height, TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) AS age FROM Patient
-        """, "PatientWithAgeMapping")
-                .getResultList();
+    public List<PatientWithAge> getPatientsWithAge() {
+        List<PatientWithAgeView> patientsWithAge = patientRepository.findAllWithAge();
+
+        return patientsWithAge.stream()
+                .map(p -> new PatientWithAge(p.getId(), p.getFirstName(), p.getLastName(), p.getWeight(), p.getHeight(), p.getAge()))
+                .toList();
+    }
+
+    public List<PatientWithAllergies> getPatientsWithAllergies() {
+        List<PatientWithAllergiesView> patientWithAllergies = patientRepository.findAllWithAllergies();
+
+        return patientWithAllergies.stream()
+                .map(p -> new PatientWithAllergies(p.getId(), p.getFirstName(), p.getLastName(), p.getWeight(), p.getHeight(),
+                        new HashSet<>(Arrays.asList(
+                                (p.getAllergies() != null) ? p.getAllergies().split(",") : new String[0])
+                        )
+                ))
+                .toList();
     }
 }
