c9f2577779f52afd245566b5559132afa1d08c00
Supavate <supavate@gmail.com>
2025-04-20 15:30:57 +0700

    Add PatientWithAllergies DTO and View

---
+++ src/main/java/com/example/elderexserver/data/patient/View/PatientWithAllergiesView.java
@@ -0,0 +1,12 @@
+package com.example.elderexserver.data.patient.View;
+
+import java.util.Set;
+
+public interface PatientWithAllergiesView {
+    Integer getId();
+    String getFirstName();
+    String getLastName();
+    Integer getWeight();
+    Integer getHeight();
+    String getAllergies();
+}
