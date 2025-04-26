9b2d6517dc72453c89e3c58535aa2608e4f8e8a9
Supavate <supavate@gmail.com>
2025-04-26 14:06:34 +0700

    Change package Name

---
+++ src/main/java/com/example/elderexserver/data/exercise/DTO/PatientWeeklyRoutineReport.java
@@ -0,0 +1,16 @@
+package com.example.elderexserver.data.exercise.DTO;
+
+public class PatientWeeklyRoutineReport {
+    Integer patient_id;
+    String first_name;
+    String last_name;
+    String exercise_name;
+    Integer year;
+    Integer week_number;
+    Integer total_sessions;
+    Integer total_reps;
+    Integer reps_goal;
+    Float percentage;
+    Integer total_calories;
+    Float average_heart_rate;
+}
