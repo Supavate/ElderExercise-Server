e5c772e6c2fbff92f95f7e3358e82fe0fbbffce2
Supavate <supavate@gmail.com>
2025-04-30 17:30:29 +0700

    change request mapping of PatientController

--- src/main/java/com/example/elderexserver/controller/PatientController.java
+++ src/main/java/com/example/elderexserver/controller/PatientController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/pat")
+@RequestMapping("/patient")
 public class PatientController {
     @Autowired
     private PatientService patientService;
