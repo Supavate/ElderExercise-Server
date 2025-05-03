5de7215e8d660be869d6af2b1f246943b946adb3
Supavate <supavate@gmail.com>
2025-05-04 06:39:04 +0700

    Add @GeneratedValue(strategy = GenerationType.IDENTITY) to id in Exercise

--- src/main/java/com/example/elderexserver/data/exercise/Exercise.java
+++ src/main/java/com/example/elderexserver/data/exercise/Exercise.java
@@ -1,11 +1,14 @@
 package com.example.elderexserver.data.exercise;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Exercise {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String description;
