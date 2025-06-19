8cf7139649376cfd91f6b6f617c9895722625a4d
supavate <supavate@gmail.com>
2025-06-19 11:53:08 +0700

    Remove unused class

--- src/main/java/com/example/elderexserver/repository/WeekDayRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.elderexserver.repository;
-
-import com.example.elderexserver.data.exercise.Week_Day;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WeekDayRepository extends JpaRepository<Week_Day, Integer> {
-}
