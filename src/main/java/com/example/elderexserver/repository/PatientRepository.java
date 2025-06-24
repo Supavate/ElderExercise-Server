a6d440c914be719b9f3cedcd77b310627abd6fe5
supavate <supavate@gmail.com>
2025-06-24 22:00:19 +0700

    Update query for added columns

--- src/main/java/com/example/elderexserver/repository/PatientRepository.java
+++ src/main/java/com/example/elderexserver/repository/PatientRepository.java
@@ -13,71 +13,112 @@ public interface PatientRepository extends JpaRepository<Patient, Integer> {
     List<PatientWithAgeView> findAllWithAge();
 
     @Query(value = """
-        Select p.id, first_name, last_name, weight, height, GROUP_CONCAT(a.name) AS allergies FROM Patient p
+        SELECT
+            p.id,
+            first_name,
+            last_name,
+            weight,
+            height,
+            GROUP_CONCAT(a.name) AS allergies
+        FROM
+            Patient p
         LEFT JOIN patient_allergy ON patient_allergy.patient_id = p.id
-        LEFT JOIN allergy a ON a.id = patient_allergy.allergy_id
-        GROUP BY p.id;
+        LEFT JOIN allergy a ON
+            a.id = patient_allergy.allergy_id
+        GROUP BY
+            p.id;
     """, nativeQuery = true)
     List<PatientWithAllergiesView> findAllWithAllergies();
 
     @Query(value = """
-        Select p.id, p.picture, p.first_name, p.last_name, g.name as gender, TIMESTAMPDIFF(YEAR, p.date_of_birth, CURDATE()) AS age FROM Patient p
-        LEFT JOIN gender g ON p.gender_id = g.id
-        WHERE p.caretaker_id =:caretakerId
+        SELECT
+            p.id,
+            p.picture,
+            p.first_name,
+            p.last_name,
+            g.name AS gender,
+            TIMESTAMPDIFF(
+                YEAR,
+                p.date_of_birth,
+                CURDATE()) AS age
+            FROM
+                Patient p
+            LEFT JOIN gender g ON
+                p.gender_id = g.id
+            WHERE
+                p.caretaker_id = :caretakerId
     """, nativeQuery = true)
     List<PatientFromCaretakerIdView> findAllByCaretakerId(int caretakerId);
 
     @Query(value = """
         SELECT
-                          p.id,
-                          p.picture,
-                          p.citizen_id,
-                          p.first_name,
-                          p.last_name,
-                          p.gender_id,
-                          g.name AS gender,
-                          DATE_FORMAT(p.date_of_birth, '%d/%m/%Y') AS date_of_birth,
-                          a.id AS allergy_id,
-                          a.name AS allergy_name,
-                          a.description AS allergy_description,
-                          bt.id AS blood_type_id,
-                          bt.type AS blood_type,
-                          p.weight,
-                          p.height,
-                          p.phone,
-                          adr.address,
-                        prov.id AS province_id,
-                        prov.name AS province,
-                        amp.id AS amphoe_id,
-                        amp.name AS amphoe,
-                        d.id AS district_id,
-                        d.name AS district,
-                          amp.zipcode AS zipcode,
-                          p.note,
-                          TIMESTAMPDIFF(
-                              YEAR,
-                              p.date_of_birth,
-                              CURDATE()) AS age
-                          FROM
-                              patient p
-                          LEFT JOIN gender g ON
-                              p.gender_id = g.id
-                          LEFT JOIN blood_type bt ON
-                              p.blood_type_id = bt.id
-                          LEFT JOIN patient_allergy pa ON
-                              p.id = pa.patient_id
-                          LEFT JOIN allergy a ON
-                              pa.allergy_id = a.id
-                          LEFT JOIN address adr ON
-                              p.address_id = adr.id
-                          LEFT JOIN district d ON
-                              adr.district_id = d.id
-                          LEFT JOIN amphoe amp ON
-                              d.amphoe_id = amp.id
-                          LEFT JOIN province prov ON
-                              amp.province_id = prov.id
-                          WHERE
-                              p.id =:id;
+            p.id,
+            p.citizen_id,
+            p.picture,
+            p.first_name,
+            p.last_name,
+            p.gender_id,
+            g.name AS gender,
+            p.blood_type_id,
+            bt.type AS blood_type,
+            p.weight,
+            p.height,
+            ROUND(p.weight / POWER(p.height / 100, 2), 3) AS BMI,
+            DATE_FORMAT(p.date_of_birth, '%d/%m/%Y') AS date_of_birth,
+            TIMESTAMPDIFF(
+                YEAR,
+                p.date_of_birth,
+                CURDATE()) AS age,
+            p.nationality_id,
+            n.name as nationality,
+            p.phone,
+            p.note,
+            p.surgical_history,
+            p.primary_hospital_id,
+            h.name as primaryHospital,
+            adr.address,
+            prov.id AS province_id,
+            prov.name AS province,
+            amp.id AS amphoe_id,
+            amp.name AS amphoe,
+            d.id AS district_id,
+            d.name AS district,
+            amp.zipcode AS zipcode,
+            fa.id AS food_id,
+            fa.name AS food_name,
+            fa.description AS food_description,
+            da.id AS drug_id,
+            da.name AS drug_name,
+            da.description AS drug_description,
+            m.id AS medicine_id,
+            m.name AS medicine_name,
+            m.description AS medicine_description
+        FROM
+            patient p
+            LEFT JOIN gender g ON
+                p.gender_id = g.id
+            LEFT JOIN blood_type bt ON
+                p.blood_type_id = bt.id
+            LEFT JOIN nationality n ON
+                p.nationality_id = n.id
+            LEFT JOIN address adr ON
+                p.address_id = adr.id
+            LEFT JOIN district d ON
+                adr.district_id = d.id
+            LEFT JOIN amphoe amp ON
+                d.amphoe_id = amp.id
+            LEFT JOIN province prov ON
+                amp.province_id = prov.id
+            LEFT JOIN patient_food_allergy pfa ON
+                p.id = pfa.patient_id
+            LEFT JOIN food_allergy fa
+                pfa.food_allergy_id = fa.id
+            LEFT JOIN patient_drug_allergy pda ON
+                p.id = pda.patient_id
+            LEFT JOIN drug_allergy da ON
+                pda.drug_allergy_id = da.id
+        WHERE
+            p.id =:id;
     """, nativeQuery = true)
     List<PatientDetailView> findPatientDetailById(int id);
 
