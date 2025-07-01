f4c31cf1a886d7fcc5fb0e9f31a9601d117e2d5e
supavate <supavate@gmail.com>
2025-07-01 12:04:28 +0700

    Added age & bmi

--- src/main/java/com/example/elderexserver/data/patient/DTO/PatientListView.java
+++ src/main/java/com/example/elderexserver/data/patient/DTO/PatientListView.java
@@ -4,6 +4,8 @@ public interface PatientListView {
     Integer getId();
     String getFirstName();
     String getLastName();
+    Integer getAge();
+    Float getBmi();
     String getCaretakerFirstName();
     String getCaretakerLastName();
     String getPicture();
